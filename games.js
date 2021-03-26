f78f8a05bc98a71c33f1a20175eb52b96484d4b8
Pablo Dávila Herrero <pablodavila314@gmail.com>
2021-03-26 01:43:40 +0100

    Add games menu

---
+++ games.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(main);
+
+function main() {
+
+    $("#tetris-card").click( function(){
+	window.location = "tetris/";
+    });
+    $("#memory-card").click( function(){
+	window.location = "memory/";
+    });
+}
+
+
